34c64716f108703ce9892b19938802a790f1a9ae
Antonio <Antoniox1996@gmail.com>
2016-01-12 05:59:30 +0100

    Corregir Respuestas

--- api/controllers/PreguntaController.js
+++ api/controllers/PreguntaController.js
@@ -6,6 +6,29 @@
  */
 
 module.exports = {
-	
-};
 
+	load: function(req, res, next) {
+		Pregunta.findOne({ 
+			where: { id: Number(req.params.PreguntaId) } 
+		}).then(function(Pregunta) {
+			if ( Pregunta ) {
+				req.Pregunta = Pregunta;
+				next();
+			}
+			else {
+				next(new Error('No existe la pregunta con el Id: ' + req.params.PreguntaId + '.'));
+			}
+		}).catch(function(Error) { next(Error); });
+	},
+
+	CorregirRespuesta: function(req, res) {
+		var RespuestaVal = (req.query.respuesta) ? req.query.respuesta : undefined, Resultado = 'Incorrecto';
+
+		if ( req.Pregunta.respuesta == RespuestaVal ) {
+			Resultado = 'Correcto';
+		}
+
+		res.json(Resultado);
+	}
+
+};
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
 
   /***************************************************************************
   *                                                                          *
@@ -45,5 +45,7 @@ module.exports.routes = {
   * for configuration options and examples.                                  *
   *                                                                          *
   ***************************************************************************/
+  
+  'GET r|º/Pregunta/(\\d+)PreguntaId/Corregir$|': [ 'PreguntaController.load', 'PreguntaController.CorregirRespuesta' ],
 
 };
